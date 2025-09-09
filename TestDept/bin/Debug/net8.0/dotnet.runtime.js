c32f3f6ac593666012dfed3d58fb81f0fe005dfc
El-Sha3rawy <81260400+El-Sha3rawy@users.noreply.github.com>
2025-09-09 03:07:22 +0300

    Delete TestDept/bin/Debug/net8.0 directory

--- TestDept/bin/Debug/net8.0/dotnet.runtime.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f750835c2bd84eb5aa4e276d3d6018ea3f70fc442d24a8d28fd567790c25d36d
-size 223202
--- TestDept/bin/Debug/net8.0/wwwroot/_framework/dotnet.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b78c45213094283bb915901ae5a9abf2301c61d3c77c145bf6e3e18d2902ce20
-size 35803
